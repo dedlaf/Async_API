3c1303f7052c1338cea63925dc8e6dc1ab721e97
Dmitrii Sobinin <Sobalchik@yandex.ru>
2024-09-12 08:04:58 +0200

    Добавлен роутер с логином

--- auth_service/main.py
+++ auth_service/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
-from api.auth.v1 import role, token_urls, user
+from api.auth.v1 import auth, role, token_urls, user
 from core.config.components import settings
 from db import redis
 
@@ -32,9 +32,10 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
     return ORJSONResponse(status_code=exc.status_code, content={"detail": exc.message})
 
 
-app.include_router(token_urls.router, prefix="/auth/token", tags=["auth"])
+app.include_router(token_urls.router, prefix="/auth/token", tags=["token"])
 app.include_router(role.router, prefix="/auth/role", tags=["roles"])
 app.include_router(user.router, prefix="/auth/user", tags=["users"])
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
 
 if __name__ == "__main__":

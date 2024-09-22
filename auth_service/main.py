c8021ca00e8c5a58d4e7ab618897d5d2533b7682
dedlaf <dedlaf.pl@mail.ru>
2024-09-22 14:03:06 +0300

    добавил jaeger

--- auth_service/main.py
+++ auth_service/main.py
@@ -12,9 +12,29 @@ from slowapi.util import get_remote_address
 from api.auth.v1 import auth, role, token_urls, user
 from core.config.components.settings import Settings
 from db import redis
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+
+
+def configure_tracer() -> None:
+    trace.set_tracer_provider(TracerProvider())
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                username="web",
+                agent_host_name="jaeger",
+                agent_port=6831
+            )
+        )
+    )
+    # Чтобы видеть трейсы в консоли
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+
 
 settings = Settings()
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -24,6 +44,7 @@ async def lifespan(app: FastAPI):
     await redis.redis.close()
 
 
+configure_tracer()
 limiter = Limiter(key_func=get_remote_address, default_limits=["1000/minute"])
 app = FastAPI(
     title=settings.project_name,
@@ -32,6 +53,9 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
+
+FastAPIInstrumentor.instrument_app(app)
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)

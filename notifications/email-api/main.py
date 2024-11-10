12d685c68ce820c05c847b946719db10a3e866ee
dedlaf <dedlaf.pl@mail.ru>
2024-11-10 14:56:56 +0300

    CRUD schemas

--- notifications/email-api/main.py
+++ notifications/email-api/main.py
@@ -1,26 +1,21 @@
+import logging
 from contextlib import asynccontextmanager
 
-from db import redis
+import aio_pika
+from api.v1 import welcome_email, content_crud, template_crud, event_crud
+from core.settings import settings
+from db import rabbitmq, redis
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
-from core.settings import settings
-import aio_pika
-from api.v1 import welcome_email
-from db import rabbitmq
-
-import logging
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    redis.redis = Redis(host='localhost', port=6379)
+    redis.redis = Redis(host="localhost", port=6379)
     print(redis.redis)
     rabbitmq.rabbitmq = await aio_pika.connect_robust(
-        host='localhost',
-        login='dedlaf',
-        password='123qwe',
-        port=5672
+        host="rabbitmq", login="dedlaf", password="123qwe", port=5672
     )
     channel = await rabbitmq.rabbitmq.channel()
     queue = await channel.declare_queue("welcome_email", durable=True)
@@ -32,19 +27,23 @@ async def lifespan(_: FastAPI):
     await rabbitmq.rabbitmq.close()
     await redis.redis.close()
 
+
 app = FastAPI(
     title=settings.project_name,
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 logger = logging.getLogger("api_logger")
-logger.info('Starting')
+logger.info("Starting")
 
 
-app.include_router(welcome_email.router, prefix="", tags=["welcome-email"])
+app.include_router(welcome_email.router, prefix="/welcome", tags=["welcome-email"])
+app.include_router(template_crud.router, prefix="/templates", tags=["template_crud"])
+app.include_router(content_crud.router, prefix="/contents", tags=["content_crud"])
+app.include_router(event_crud.router, prefix="/events", tags=["events_crud"])
 
 
 if __name__ == "__main__":
